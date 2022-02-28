dcfc714807d6a0fbf0416374c0fb2e798ece841d
peiyuwu97 <ollyc1030@gmail.com>
2022-03-01 01:38:38 +0800

    rwd

--- js/reserve.js
+++ js/reserve.js
@@ -44,24 +44,50 @@ function uploadFile(){
             };
 
             img.src = reader.result;
-    
+            
+            // 上傳的照片
             let myimgs = document.getElementsByClassName("myimg");
 
-            // console.log(myimgs.length);
             if(myimgs.length == 3){
                 div_upload.remove()
                 file_label_rwd.remove()
             }
-    
+            if(myimgs.length != 0){
+
+                // 第二頁 顯示照片第一張在明細
+                // console.log(document.getElementById("d_pic"));
+                console.log(document.getElementsByClassName("btn_div"));
+                // let dp_img = document.createElement('img');
+                // dp_img.src = myimgs[0].src;
+                // document.getElementsByClassName("d_pic")[0].appendChild(dp_img);
+            }
         }
         reader.readAsDataURL(file);
     }
 
-}
+};
 
+// function showMyImg(){
+//         let myimgs = document.getElementsByClassName("myimg"); //陣列
+//         console.log(myimgs.length);
+//         if(myimgs.length != 0){
+//         };
+        // 上傳的照片
+        // 第二頁 顯示照片第一張在明細
+        // console.log(myimgs[0].src);
+        // let dp_img = document.createElement('img');
+        // dp_img.src = myimgs[0].src;
+        // document.getElementsByClassName("d_pic")[0].appendChild(dp_img);
+// }
+ 
 
 function init(){
     uploadFile();
 }
 
 window.addEventListener('load',init);
+
+
+
+// ----------
+
