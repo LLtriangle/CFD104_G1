2e7f61e2f15ddaaecc4c611b861b6272e5eb6cab
vavelvet <annie870101annie@gmail.com>
2022-03-02 09:40:31 +0800

    方案嘗試可以拖拉失敗

--- js/service.js
+++ js/service.js
@@ -1,29 +1,14 @@
-// 未完成 : 方案拖拉、案例分享動畫
-var box=$(".item_group");
-var body=$('body');
-var index=0;
-var x1;
-box.mousedown(function(){
-	index=1;       //滑鼠按下才能觸發onmousemove方法
-	var x=event.clientX;   //滑鼠點選的座標值，x
-	var left= this.style.left;
-	left=left.substr(0,left.length-2);  //去掉px
-	x1=parseInt(x-left);
+// <!-- 貓貓貓貓頭鷹啦 -->
+// <script src="OwlCarousel/owl.carousel.min.js"></script>
+$('.owl-carousel').owlCarousel({
+	items:1,
+	autoHeight:true,
+	// loop:true,
+	// margin:10,
+	autoWidth:true,
+	items:4
 });
-box.mousemove(function(){
-	if(index===1){
-		this.style.left=event.clientX-x1+'px';
-		if(this.style.left.substr(0,this.style.left.length-2)<0){
-			this.style.left=0;
-		};
-		if(this.style.left.substr(0,this.style.left.length-2)>150){
-			this.style.left='150px';
-		};
-	}
-});
-box.mouseup(function(){
-	index=0;
-});
-body.mouseup(function(){
-	index=0;
-});
+AutoHeight.Defaults = {
+	autoHeight: false,
+	autoHeightClass: 'owl-height',
+};
