bbbfca5f6198c87527ade3d9487ed70e359af603
vavelvet <annie870101annie@gmail.com>
2022-03-04 16:49:08 +0800

    card、遊戲頁改一點、服務頁案例樣式

--- js/service.js
+++ js/service.js
@@ -1,34 +1,37 @@
 $(window).on('load',function(){
   
-	const case01 = gsap.timeline({
+	const serveCase = gsap.timeline({
 		scrollTrigger:{
-			trigger: '.case01',
+			trigger: '.serve_case',
 			start: 'center center',
 			pin: true,
-			// markers:true,
 			scrub: true,
-		}
-	});
-	const case02 = gsap.timeline({
-		scrollTrigger:{
-			trigger: '.case02',
-			start: 'center center',
-			pin: true,
 			// markers:true,
-			scrub: true,
-		}
-	});
-	const case03 = gsap.timeline({
-		scrollTrigger:{
-			trigger: '.case03',
-			start: 'center center',
-			pin: true,
-			// markers:true,
-			scrub: true,
 		}
 	});
 	
-	case01.from('.case01 .pic img:last-child',{y:600});
-	case02.from('.case02 .pic img:last-child',{y:600});
-	case03.from('.case03 .pic img:last-child',{y:600});
+	serveCase.fromTo('.case01',{
+		y:100,
+	},{
+		y:100,
+		ease: "elastic.out(1, 0.3)",
+	}).to('.case01 .pic .afterPic',{
+		// x:-700,
+		width:500,
+	}).fromTo('.case02',{
+		y:1000
+	},{
+		y:100,
+	}).to('.case02 .pic .afterPic',{
+		// x:1700,
+		width:500,
+	}).fromTo('.case03',{
+		y:1000
+	},{
+		y:100,
+	}).to('.case03 .pic .afterPic',{
+		// x:-700,
+		width:500,
+	});
+
 });
