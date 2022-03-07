e36375d3950d214d0e0fc435d341e742dfda3888
vavelvet <annie870101annie@gmail.com>
2022-03-07 22:45:01 +0800

    服務頁: 案例對比區塊加上便利貼

--- js/service.js
+++ js/service.js
@@ -11,42 +11,57 @@ $(window).on('load',function(){
 	});
 	
 	serveCase.fromTo('.case01',{
-		y:100,
+		y:150,
 	},{
-		y:100,
-		ease: "elastic.out(1, 0.3)",
+		y:150,
 	}).to('.case01 .pic .afterPic',{
 		width:500,
 	},">").to('.case01 .pic .handle',{
 		left:500,
 	},"<").to('.case01 .txt .afterTxt',{
 		height:500,
-	},"<").fromTo('.case02',{
-		y:1000
+	},"<").fromTo('.case01 .bookmark',{
+		y:1000,
 	},{
-		y:100,
-	}).to('.case02 .pic .afterPic',{
+		y:-30,
+	}).fromTo('.case02',{
+		y:1000,
+	},{
+		y:150,
+	}).to('.case01',{
+		y:120,
+	},"<").to('.case02 .pic .afterPic',{
 		width:500,
 	},">").to('.case02 .pic .handle',{
 		left:500,
 	},"<").to('.case02 .txt .afterTxt',{
 		height:500,
-	},"<").fromTo('.case03',{
-		y:1000
+	},"<").fromTo('.case02 .bookmark',{
+		y:1000,
 	},{
-		y:100,
-	}).to('.case03 .pic .afterPic',{
+		y:-30,
+	}).fromTo('.case03',{
+		y:1000,
+	},{
+		y:150,
+	}).to('.case01',{
+		y:90,
+	},"<").to('.case02',{
+		y:120,
+	},"<").to('.case03 .pic .afterPic',{
 		width:500,
 	},">").to('.case03 .pic .handle',{
 		left:500,
 	},"<").to('.case03 .txt .afterTxt',{
 		height:500,
-	},"<");
+	},"<").fromTo('.case03 .bookmark',{
+		y:1000,
+	},{
+		y:-30,
+	});
 
 
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-	delta = 0;
-
+// 前後對比與滑鼠位置
     $('.case .pic').on('mousemove', function(e) {
 		$(this).children('.afterPic').removeClass('slowMove');
 		$(this).children('.handle').removeClass('slowMove');
