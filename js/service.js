9e290bf795e36f802ef002dc68120e4f2af82ea5
vavelvet <annie870101annie@gmail.com>
2022-03-07 11:59:51 +0800

    服務頁: 案例對比完成

--- js/service.js
+++ js/service.js
@@ -16,22 +16,44 @@ $(window).on('load',function(){
 		y:100,
 		ease: "elastic.out(1, 0.3)",
 	}).to('.case01 .pic .afterPic',{
-		// x:-700,
 		width:500,
-	}).fromTo('.case02',{
+	},">").to('.case01 .pic .handle',{
+		left:500,
+	},"<").to('.case01 .txt .afterTxt',{
+		height:500,
+	},"<").fromTo('.case02',{
 		y:1000
 	},{
 		y:100,
 	}).to('.case02 .pic .afterPic',{
-		// x:1700,
 		width:500,
-	}).fromTo('.case03',{
+	},">").to('.case02 .pic .handle',{
+		left:500,
+	},"<").to('.case02 .txt .afterTxt',{
+		height:500,
+	},"<").fromTo('.case03',{
 		y:1000
 	},{
 		y:100,
 	}).to('.case03 .pic .afterPic',{
-		// x:-700,
 		width:500,
-	});
+	},">").to('.case03 .pic .handle',{
+		left:500,
+	},"<").to('.case03 .txt .afterTxt',{
+		height:500,
+	},"<");
+
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	delta = 0;
+
+    $('.case .pic').on('mousemove', function(e) {
+		$(this).children('.afterPic').removeClass('slowMove');
+		$(this).children('.handle').removeClass('slowMove');
+        $(this).children('.handle').css('left',`${Math.max(0,e.offsetX)}px`);
+        $(this).children('.afterPic').css('width',`${e.offsetX}px`);
+		$(this).children('.afterPic').addClass('slowMove');
+		$(this).children('.handle').addClass('slowMove');
+    });
 
 });
