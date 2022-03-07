2c21cae7f9da679b65600a65f9bbc5adbfafd4bb
vavelvet <annie870101annie@gmail.com>
2022-03-07 19:26:03 +0800

    遊戲頁: 垃圾桶衣櫃可顯示為空，衣櫃點擊優化，物品大概不破圖了

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-// 未完成:拖曳物件、結果判斷
+// 未完成:拖曳物件
 function init() {
     // 拖放物件
     // $('#item_group img').draggable({
@@ -50,13 +50,20 @@ function init() {
         if($('#item_group img.selected').length > 0) {
             // 條件:有選擇物品，收進衣櫃
             $('#wardrobe #hiding').append($('#item_group img.selected'));
+            $('#wardrobe').removeClass('empty');
         }else{
+            // 條件:未選擇物品，打開衣櫃
             $('#wardrobe').toggleClass('open');
             $('#pull_arrow').css('display','none');
+            if($('#wardrobe #hiding img').length == 0){
+                // 條件:衣櫃是空的，顯示為空
+                $('#wardrobe').addClass('empty');
+            };
         };
         finishButton();   // 送出按鈕disabled切換
     };
     $('#wardrobe > img').on('click',openWardrobe);
+    $('#wardrobe #inside').on('click',openWardrobe);
     $('#pull_arrow').on('click',openWardrobe);
 
     // 點擊垃圾桶
@@ -64,27 +71,38 @@ function init() {
         if($('#item_group img.selected').length > 0) {
             // 條件:有選擇物品，丟進垃圾桶
             $('#trashcan #garbage').append($('#item_group img.selected'));
+            $('#trashcan').removeClass('empty');
         }else{
+            // 條件:未選擇物品，打開垃圾桶
             $('#trashcan').toggleClass('open');
+            if($('#trashcan #garbage img').length == 0){
+                // 條件:垃圾桶是空的，顯示為空
+                $('#trashcan').addClass('empty');
+            };
         };
         finishButton();   // 送出按鈕disabled切換
     });
 
-    // 點擊收納物
+    // 點擊衣櫃內收納物
     $(document).on('click','#hiding img',function(e){
         $(this).prependTo($('#item_group'));
-        // $('#item_group label img.selected').prop('checked',false);
+        if($('#wardrobe #hiding img').length == 0){
+            // 條件:衣櫃是空的，顯示為空
+            $('#wardrobe').addClass('empty');
+        };
     });
 
     // 點擊垃圾
     $(document).on('click','#garbage img',function(e){
         $(this).prependTo($('#item_group'));
-        // $('#item_group label img.selected').prop('checked',false);
+        if($('#trashcan #garbage img').length == 0){
+            // 條件:垃圾桶是空的，顯示為空
+            $('#trashcan').addClass('empty');
+        };
     });
     
     // 開始測驗，關閉說明視窗
     function switchLight(e){
-        // $(this).parent().css('display','none');
         $(this).parent().slideToggle(500,'easeInCirc');
     };
     $('.text_ill button').on('click',switchLight);
@@ -101,16 +119,16 @@ function init() {
 
     // 再來一次，跳出說明視窗(遊玩方法)，物品歸位
     function openIll(){
-        $(".text_ill .intr").slideToggle(1000,'easeOutBounce');
-        $('.text_result .paper').scrollTop(0);
-        $(".text_result .typewriter").css('bottom','-50%');
-        $(".text_result .paper h2").css('margin-top','100%');
-        $(".text_result").css('display','none');
-        $('#finish').attr('disabled',true);
-        $('.pullbox_group .pullbox img').appendTo($('#item_group'));
-        $('#garbage img').appendTo($('#item_group'));
-        $('#hiding img').appendTo($('#item_group'));
-        $('#item_group img').removeClass('selected');
+        $(".text_ill .intr").slideToggle(1000,'easeOutBounce'); // 跳出說明視窗
+        $('#pull_arrow').css('display','block');    // 回復衣櫃上提示手指
+        $('.text_result .paper').scrollTop(0);                  // 回復紙張捲動高度
+        $(".text_result .paper h2").css('margin-top','100%');   // 回復紙張高度(animate可再次啟動)
+        $(".text_result").css('display','none');    // 藏起測驗結果
+        $('#finish').attr('disabled',true);         // 藏起完成擺放按鈕
+        $('.pullbox_group .pullbox img').appendTo($('#item_group'));    // 清空擺放物品
+        $('#garbage img').appendTo($('#item_group'));   // 清空垃圾桶
+        $('#hiding img').appendTo($('#item_group'));    // 清空衣櫃
+        $('#item_group img').removeClass('selected');   // 回復物品未選取狀態
     };
     $('#again').on('click',openIll);
     
