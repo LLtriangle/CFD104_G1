68e50cbdf2781d91e941c0f16738835465b177ec
peiyuwu97 <ollyc1030@gmail.com>
2022-03-11 10:10:41 +0800

    註冊ok

--- php/cus_vali.php
+++ php/cus_vali.php
@@ -1,9 +1,20 @@
 <?php
-session_start();
+try{
+	// require_once("../connect_cfd104g1.php"); // 上線用
+	require_once("connect.php"); // 開發用
 
-if(isset($_SESSION["EMAIL"]) == true){
-    echo $_SESSION["CUS_NAME"];
-    echo $_SESSION["EMAIL"];
+  $sql = "select * from cus where EMAIL = ?";
+  $member = $pdo->prepare($sql);
+  $member->bindValue(1,$_GET["cusId"]);
+  $member->execute(); //執行
+
+
+  if( $member->rowCount() != 0){ //有找到 => 此帳號已存在, 不可用
+    echo "此帳號已存在, 不可用";  //xhr.responseText : 伺服器的回應資料,回應的內容為一個字串
+  }else{ //此帳號可使用
+    echo "此帳號可使用";
+  } 
+}catch(PDOException $e){
+  echo "error";
 }
-
 ?>
