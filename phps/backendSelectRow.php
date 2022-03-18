faad43ad9babdd94d173277e3493150e3f79c35e
LLtriangle <arbeitlee21@gmail.com>
2022-03-18 09:53:45 +0800

    後臺除了要join的資料已經先抓到了 但是還有很多功能在前方等著哈QQ

--- phps/backendSelectRow.php
+++ phps/backendSelectRow.php
@@ -2,14 +2,14 @@
 try {
 	require_once("connect.php"); // 開發用
 
-    if ($_GET['data'] == "emp") {
-		$sql = "select * from $_GET['tableName'] where EMP_NO = $_GET['editNo'] ";
+    if ($_GET['tableName'] == "emp") {
+		$sql = "select EMP_NO, EMP_NAME, EMP_EMAIL, EMP_TEL, EMP_STATE, JOB, EMP_ADD, HIREDATE, INTRO from {$_GET['tableName']} where EMP_NO = {$_GET['editNo']} ";
 		
-	}elseif ($_GET['data'] == "cus"){
-		$sql = "select * from $_GET['tableName'] where CUS_NO = $_GET['editNo'] ";
+	}elseif ($_GET['tableName'] == "cus"){
+		$sql = "select CUS_NO, JOIN_DATE, EMAIL, STATE, CUS_NAME, CUS_TEL, CUS_ADD, SEX from {$_GET['tableName']} where CUS_NO = {$_GET['editNo']} ";
 		
-	}elseif ($_GET['data'] == "result"){
-		$sql = "select * from $_GET['tableName'] where RESULT_NO = $_GET['editNo'] ";
+	}elseif ($_GET['tableName'] == "result"){
+		$sql = "select * from {$_GET['tableName']} where RESULT_NO = {$_GET['editNo']} ";
 
 		// $sql = "select R.RESULT_NO no, R.RESULT_NAME, P.PLAN_NAME,  Pr1.PRD_NAME prd1, Pr2.PRD_NAME prd2, Pr3.PRD_NAME prd3
 		// from result R join plan P on R.PLAN_NO = P.PLAN_NO
@@ -17,28 +17,28 @@ try {
         //              join prd Pr2 on R.PRD_NO2 = Pr2.PRD_NO
         //              join prd Pr3 on R.PRD_NO3 = Pr3.PRD_NO";
 
-	}elseif ($_GET['data'] == "prd"){
-		$sql = "select * from $_GET['tableName'] where PRD_NO = $_GET['editNo'] ";
+	}elseif ($_GET['tableName'] == "prd"){
+		$sql = "select * from {$_GET['tableName']} where PRD_NO = '{$_GET['editNo']}' ";
 		
-	}elseif ($_GET['data'] == "ord"){
-		$sql = "select * from $_GET['tableName'] where ORD_NO = $_GET['editNo'] ";
+	}elseif ($_GET['tableName'] == "ord"){
+		$sql = "select * from {$_GET['tableName']} where ORD_NO = '{$_GET['editNo']}' ";
 		
-	}elseif ($_GET['data'] == "plan"){
-		$sql = "select * from $_GET['tableName'] where PLAN_NO = $_GET['editNo'] ";
+	}elseif ($_GET['tableName'] == "plan"){
+		$sql = "select * from {$_GET['tableName']} where PLAN_NO = '{$_GET['editNo']}' ";
+
+	}elseif ($_GET['tableName'] == "sao"){
+		$sql = "select * from {$_GET['tableName']} where PLAN_NO = '{$_GET['editNo']}' ";
+			
+	}elseif ($_GET['tableName'] == "casetable"){
+		$sql = "select * from {$_GET['tableName']} where CASE_NO = '{$_GET['editNo']}' ";
 		
-	}elseif ($_GET['data'] == "casetable"){
-		$sql = "select * from $_GET['tableName'] where CASE_NO = $_GET['editNo'] ";
+	}elseif ($_GET['tableName'] == "event"){
+		$sql = "select * from {$_GET['tableName']} where COUPON = '{$_GET['editNo']}' ";
 		
-	}elseif ($_GET['data'] == "event"){
-		$sql = "select * from $_GET['tableName'] where COUPON = $_GET['editNo'] ";
-		
-	}elseif ($_GET['data'] == "columntable"){
-		$sql = "select * from $_GET['tableName'] where COLUMN_NO = $_GET['editNo'] ";
+	}elseif ($_GET['tableName'] == "columntable"){
+		$sql = "select * from {$_GET['tableName']} where COLUMN_NO = '{$_GET['editNo']}' ";
 		
 	};
-
-
-
 
 	$cases = $pdo->query($sql);	
 	$caseRows = $cases->fetch(PDO::FETCH_ASSOC);
