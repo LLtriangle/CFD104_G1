77589f652fc27251ddcfc39ee513f5b4961160c6
peiyuwu97 <ollyc1030@gmail.com>
2022-03-21 20:11:30 +0800

    no message

--- phps/getEmpSch.php
+++ phps/getEmpSch.php
@@ -1,11 +1,14 @@
 <?php 
+session_start();
 try {
 	// require_once("../connect_cfd104g1.php"); // 上線用
 	require_once("connect.php"); // 開發用
-	$sql = " SELECT EMP_NO,DAY_OFF,SCH_TIME FROM sch WHERE EMP_NO='{$_GET['data']}'
-    UNION
-    SELECT EMP_NO,SAO_DATE,SAO_TIME FROM sao WHERE EMP_NO='{$_GET['data']}' ";
-	$info = $pdo->query($sql);
+	// $sql = " SELECT * FROM sao WHERE EMP_NO='$_SESSION["EMP_NO"]' ";
+	$sql = "select * FROM sch where EMP_NO = :EMP_NO";
+
+	$info = $pdo->prepare($sql);
+	$info->bindValue(":EMP_NO",$_SESSION["EMP_NO"]);
+	$info->execute();
 	$infos = $info->fetchAll(PDO::FETCH_ASSOC);
 	echo json_encode($infos);
 } catch (PDOException $e) {
