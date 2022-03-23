992e96df6ba7c067037d181161f72f872c95e13f
peiyuwu97 <ollyc1030@gmail.com>
2022-03-23 20:17:52 +0800

    add coupon.php

---
+++ phps/coupon.php
@@ -0,0 +1,16 @@
+<?php
+try{
+	// require_once("../connect_cfd104g1.php"); // 上線用
+    require_once("connect.php"); // 開發用
+
+  $sql = "select * FROM coupon WHERE CUS_NO = :CUS_NO;";
+  $cp = $pdo->prepare($sql);
+  $cp -> bindValue(":CUS_NO",$_SESSION["CUS_NO"]);
+  $cp -> execute(); //執行
+  $cps = $cp->fetchAll(PDO::FETCH_ASSOC);
+  echo json_encode($cps);
+
+}catch(PDOException $e){
+  echo "error";
+}
+?>
