574ee44dfbbf76e8c2c17367fb5c24842d98496b
LLtriangle <arbeitlee21@gmail.com>
2022-03-24 10:33:45 +0800

    趕快把丟上去~

--- phps/backend.php
+++ phps/backend.php
@@ -49,8 +49,8 @@ try {
 	echo json_encode($caseRows);
 
 } catch (PDOException $e) {
-	echo "錯誤原因 : ", $e->getMessage(), "<br>";
-	echo "錯誤行號 : ", $e->getLine(), "<br>";
+	// echo "錯誤原因 : ", $e->getMessage(), "<br>";
+	// echo "錯誤行號 : ", $e->getLine(), "<br>";
 	// echo "系統錯誤, 請通知系統維護人員<br>";
 };
 ?>
